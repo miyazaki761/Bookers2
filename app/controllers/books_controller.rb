d2307f7de43f53bda80643aed4668cdef33024c6
a <miyazaki.a0806@gmail.com>
2019-12-06 18:09:56 +0900

    update

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.user_id = curret_user.id
+		@book.save
+		redirect_to books_path
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+end
