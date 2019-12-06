a618982850d15104d571801ecadab9e44a963840
a <miyazaki.a0806@gmail.com>
2019-12-06 18:34:01 +0900

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,19 @@ class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
 		@book.user_id = curret_user.id
-		@book.save
-		redirect_to books_path
+		if @book.save
+		  redirect_to books_path
+		else
+			render :new
+		end
 	end
 
 	def index
+		@books = Book.all
 	end
 
 	def show
+		@book = Book.find(@params[:id])
 	end
 
 end
